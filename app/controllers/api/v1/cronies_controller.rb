a0b60e5aff67551c00a4c046d383c52d94d54af7
Timilehin Emmanuel <Harriemannie@gmail.com>
2021-03-31 21:42:23 +0100

    create update action and api end point

--- app/controllers/api/v1/cronies_controller.rb
+++ app/controllers/api/v1/cronies_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::CroniesController < Api::V1::BaseController
-    before_action :set_crony, only: [: show ] 
+    acts_as_token_authentication_handler_for User, except: [ :index, :show ]
+    before_action :set_crony, only: [ :show, :update ] 
 
     def index
       @cronies = policy_scope(Crony)
@@ -8,11 +9,29 @@ class Api::V1::CroniesController < Api::V1::BaseController
     def show
     end
 
+    def update
+        if @crony.update(crony_params)
+            render :show
+        else
+            render_error
+        end
+    end
+
     private
     
     def set_crony
         @crony = Crony.find(params[:id])
         authorize @crony
     end
+
+    def crony_params
+        params.require(:crony).permit(:name, :address, :nickname, :category, :decription)
+    end
+
+    def render_error
+        render json: { errors: @crony.errors.full_messages },
+            status: :unprocessable_entity
+    end
+
   end
   
